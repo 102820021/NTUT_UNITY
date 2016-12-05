7d0d3284713496c849bec42aab82c9ccdd75b02f
102820021 <102820021wsx987@gmail.com>
2016-12-05 16:55:44 +0800

    Complete Game(connecting menu)

---
+++ Assets/Script/Menu.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour {
+    public GameObject m_menu;
+    public Text m_gameResult;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void WinGame()
+    {
+        m_menu.SetActive(true);
+        m_gameResult.text = "WIN";
+    }
+
+    public void LoseGame()
+    {
+        m_menu.SetActive(true);
+        m_gameResult.text = "LOSE";
+    }
+
+    public void StartGame()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+
+    }
+
+    public void ReplayGame()
+    {
+
+    }
+}
--- Assets/Script/ProtectedObject.cs
+++ Assets/Script/ProtectedObject.cs
@@ -3,10 +3,12 @@ using System.Collections;
 
 public class ProtectedObject : MonoBehaviour {
     public GameObject m_protectedObject;
+    public Menu m_menu;
+    private bool isMoving = true;
 	// Use this for initialization
 	void Start () {
-	
-	}
+        m_menu = GameObject.FindGameObjectsWithTag("Menu")[0].GetComponent<Menu>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -15,18 +17,23 @@ public class ProtectedObject : MonoBehaviour {
 
     void OnTriggerEnter(Collider other)
     {
-        if(other.tag != "FingerPosition")
+        if(other.tag == "Enemy")
         {
-            Debug.Log("End Game");
+            m_menu.LoseGame();
+            Time.timeScale = 0;
+//            isMoving = false;
         }
-        else
+        if(other.tag == "End")
         {
-            Debug.Log(other.name);
+            m_menu.WinGame();
+            Time.timeScale = 0;
+//            isMoving = false;
         }
     }
 
     void Move()
     {
-        m_protectedObject.transform.position = new Vector3(m_protectedObject.transform.position.x, m_protectedObject.transform.position.y + 0.05f, m_protectedObject.transform.position.z);
+        if(isMoving)
+            m_protectedObject.transform.position = new Vector3(m_protectedObject.transform.position.x, m_protectedObject.transform.position.y + 0.05f, m_protectedObject.transform.position.z);
     }
 }
