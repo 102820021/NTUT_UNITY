aca74e4af641bf48623549ce07e088285c8da9db
102820021 <102820021wsx987@gmail.com>
2016-12-30 18:40:17 +0800

    Start Menu

---
+++ Assets/Script/MenuManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+    public GameObject MainMenu;
+    public GameObject CurrentMenu;
+	// Use this for initialization
+	void Start () {
+        MainMenu.active = true;
+//        CurrentMenu.active = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ShowMenu(GameObject menu)
+    {
+        if (!MainMenu.name.Equals(menu.name))
+        {
+            Debug.Log("*");
+            MainMenu.active = false;
+            MainMenu = menu;
+            menu.active = true;
+        }
+    }
+}
